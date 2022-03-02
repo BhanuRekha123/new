a14581a1c6982e23394ff793986a504cab931b4a
BhanuRekha123 <bhanurekha517@gmail.com>
2022-03-02 11:38:45 +0530

    Responsive for 2XL

--- src/About.js
+++ src/About.js
@@ -3,11 +3,11 @@ import React from "react";
 function About() {
   return (
     <div className=" text-center">
-      <h1 className="mt-32 mb-8 uppercase text-4xl font-semibold text-white">
+      <h1 className="2xl:mt-32 mt-28 2xl:mb-8 mb-5 uppercase 2xl:text-4xl text-3xl font-semibold text-white">
         What are ReactJS and TailwindCSS?
       </h1>
       <div className="flex flex-row space-x-8 justify-center items-center">
-        <div class="block p-6 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+        <div class="block 2xl:p-6 p-4 2xl:max-w-lg max-w-md 2xl:max-h-lg max-h-md bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
           <h5 class="mb-2 text-2xl font-bold text-mycolor">ReactJS</h5>
           <p class="text-gray-900 text-justify">
             React is a JavaScript-based UI development library. Facebook and an
@@ -24,13 +24,13 @@ function About() {
             4). Dedicated tools for easy debugging.
           </p>
         </div>
-        <div class="block p-6 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 ">
+        <div class="block 2xl:p-6 p-4 2xl:max-w-lg max-w-md 2xl:max-h-lg max-h-md bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
           <h5 class="mb-2 text-2xl font-bold text-mycolor ">TailwindCSS</h5>
           <p class="text-gray-900 text-justify">
             Tailwind is a utility-first CSS framework. In contrast to other CSS
             frameworks like Bootstrap or Materialize CSS it doesn’t come with
-            predefined components. Instead Tailwind CSS operates on a lower
-            level and provides you with a set of CSS helper classes.
+            predefined components. <span className="hidden 2xl:block">Instead Tailwind CSS operates on a lower
+            level and provides you with a set of CSS helper classes.</span>
             <br />
             <br />
             1). Minimum lines of Code in CSS file.
--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,9 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <div className=" pt-44 text-white flex flex-row justify-center text-center">
+      <div className=" 2xl:pt-44 pt-32 text-white flex flex-row justify-center text-center">
         <div className="w-2/3">
-          <h1 className="font-bold text-6xl">WELCOME TO MY FIRST WEBSITE</h1>
+          <h1 className="font-bold 2xl:text-6xl text-4xl">WELCOME TO MY FIRST WEBSITE</h1>
           <p className="mt-6 mb-6 text-justify">This is my first website using React and Tailwind CSS. React is an open-source JavaScript library that is used for building user interfaces specifically for single-page applications. React also allows us to create reusable UI components. Tailwind CSS is basically a utility-first CSS framework for rapidly building custom user interfaces. It is a highly customizable, low-level CSS framework that gives you all of the building blocks you need to build bespoke designs without any annoying opinionated styles you have to fight to override.</p>
           <a href="./Myprofile" className="mybtn mx-10">LOGIN</a>
           <a href="./Myprofile" className="mybtn mx-10">SIGN UP</a>
